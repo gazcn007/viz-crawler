0523a8fb926ef5673dc330ca077cdf1f1c9a1bbe
gazcn007-tableau <sliu@tableau.com>
2017-11-09 14:04:34 -0800

    add shee-url-parser.js, every time you run it it will parse more urls into your json file"

--- sheet-url-parser.js
+++ sheet-url-parser.js
@@ -1,29 +1,44 @@
 const CDP = require('chrome-remote-interface');
 const fsPath = require('fs-path');
 
+function triggerEventOnPage(selector, eventName, document) {
+        var event;
+        var element = document.querySelector(selector);
+
+        event = document.createEvent("Event");
+        event.initEvent(eventName, true, true);
+        event.memo = memo || { };
+
+        element.dispatchEvent(event);
+}
+
 CDP({
     host: 'localhost',
     port: 9222,
 }, async client => {
-    const {Page, DOM, Input} = client;
+    const {Page, DOM, Input, Runtime} = client;
 
     await Page.enable();
+    await Runtime.enable();
     console.log('The Automation Parser is hacking the aws-tabint')
 
-    // 1. keep scrolling down till all the views are shown on the webpage
-    await Input.synthesizeScrollGesture({
-        x: 500, 
-        y: 500, 
-        yDistance: -15000, 
-        speed: -15000, 
-        repeatCount: 5, 
-        repeatDelayMs: 2000});
+    await Runtime.evaluate({
+        expression: "function triggerEventOnPage(selector, eventName) {\
+        var event;\
+        var element = document.querySelector(selector);\
+        event = document.createEvent('Event');\
+        event.initEvent('click', true, true);\
+        element.dispatchEvent(event);\
+        }\
+        triggerEventOnPage('.pager-next > a','click')"
+    });
 
-    // 2. Parse the DOM 
+    // 3. Parse the DOM 
     const rootElement = await DOM.getDocument();
+
     const { root: { nodeId } } = rootElement;
     const { nodeIds: linkIDs } = await DOM.querySelectorAll({
-        selector: ".workbook-title > a",
+        selector: ".media-viz__thumbnail >a",
         nodeId,
     });
 
@@ -31,7 +46,6 @@ CDP({
     const attributes = await Promise.all(linkIDs.map((ID) =>
         DOM.getAttributes({ nodeId: ID })
     ));
-
     // Atrributes are returned in single array and item pairs
     const links = attributes
         .map(x => x.attributes)
@@ -43,9 +57,9 @@ CDP({
 
     // Use set to get unique items only
     const uniqueLinks = new Set([...links]);
-    console.log(links);
+    console.log(links.length);
     var objToWrite = { urls:links.map(e=>{
-        return "https://public-aws-poc.dev.tabint.net/profile/qa.tableau"+e
+        return e
     }) };
     fsPath.writeFile('./profile-urls.json', JSON.stringify(objToWrite,null,2), function (err){
         if(err) throw err;
