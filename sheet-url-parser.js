5e2c700478d40d99139a6939c9a72e49c9f5fa25
Troy <troy@v1020-wn-71-63.campus-dynamic.uwaterloo.ca>
2017-11-14 11:25:58 -0500

    [sheet-url-parser] Accept command line param for desired URL crawling amount

--- sheet-url-parser.js
+++ sheet-url-parser.js
@@ -1,6 +1,8 @@
 const CDP = require('chrome-remote-interface');
 const fsPath = require('fs-path');
 
+const crawlingAmount = process.argv[2] || 100;
+
 function triggerEventOnPage(selector, eventName, document) {
         var event;
         var element = document.querySelector(selector);
@@ -21,46 +23,48 @@ CDP({
     await Page.enable();
     await Runtime.enable();
     console.log('The Automation Parser is Hacking the Gallery Page');
+    let objToWrite = {
+      urls: []
+    };
+    while (objToWrite.urls.length <= crawlingAmount) {
+      await Runtime.evaluate({
+          expression: "function triggerEventOnPage(selector, eventName) {\
+          var event;\
+          var element = document.querySelector(selector);\
+          event = document.createEvent('Event');\
+          event.initEvent('click', true, true);\
+          element.dispatchEvent(event);\
+          }\
+          triggerEventOnPage('.pager-next > a','click')"
+      });
 
-    await Runtime.evaluate({
-        expression: "function triggerEventOnPage(selector, eventName) {\
-        var event;\
-        var element = document.querySelector(selector);\
-        event = document.createEvent('Event');\
-        event.initEvent('click', true, true);\
-        element.dispatchEvent(event);\
-        }\
-        triggerEventOnPage('.pager-next > a','click')"
-    });
+      // 3. Parse the DOM
+      const rootElement = await DOM.getDocument();
 
-    // 3. Parse the DOM 
-    const rootElement = await DOM.getDocument();
+      const { root: { nodeId } } = rootElement;
+      const { nodeIds: linkIDs } = await DOM.querySelectorAll({
+          selector: ".media-viz__thumbnail >a",
+          nodeId,
+      });
 
-    const { root: { nodeId } } = rootElement;
-    const { nodeIds: linkIDs } = await DOM.querySelectorAll({
-        selector: ".media-viz__thumbnail >a",
-        nodeId,
-    });
+      // Get each element attributes
+      const attributes = await Promise.all(linkIDs.map((ID) =>
+          DOM.getAttributes({ nodeId: ID })
+      ));
+      // Atrributes are returned in single array and item pairs
+      const links = attributes
+          .map(x => x.attributes)
+          .filter(x => x.includes("href"))
+          .map((attrs) => {
+              const index = attrs.indexOf("href");
+              return attrs[index + 1];
+          });
 
-    // Get each element attributes
-    const attributes = await Promise.all(linkIDs.map((ID) =>
-        DOM.getAttributes({ nodeId: ID })
-    ));
-    // Atrributes are returned in single array and item pairs
-    const links = attributes
-        .map(x => x.attributes)
-        .filter(x => x.includes("href"))
-        .map((attrs) => {
-            const index = attrs.indexOf("href");
-            return attrs[index + 1];
-        });
-
-    // Use set to get unique items only
-    const uniqueLinks = new Set([...links]);
-    console.log("Total Links hacked is now increased to "+links.length);
-    var objToWrite = { urls:links.map(e=>{
-        return e
-    }) };
+      // Use set to get unique items only
+      const uniqueLinks = new Set([...links]);
+      objToWrite.urls = objToWrite.urls.concat([...uniqueLinks]);
+      console.log("Total Links hacked is now increased to "+objToWrite.urls.length);
+    }
     fsPath.writeFile('./profile-urls.json', JSON.stringify(objToWrite,null,2), function (err){
         if(err) throw err;
     })
